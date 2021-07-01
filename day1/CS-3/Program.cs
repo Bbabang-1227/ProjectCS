38dcbfbb6dc24cfd5b177bbe06259b8418f52392
Bbabang-1227 <guswns2811@gmail.com>
2021-07-01 18:22:42 +0900

    주성 수업
    1

---
+++ day1/CS-3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections;
+
+using fs = FirstNamespace;
+using ss = SecondNamespace;
+
+namespace FirstNamespace
+{
+    class Test
+    {
+        public void f1()
+        {
+            WriteLine(1);
+        }
+    }
+}
+
+namespace SecondNamespace
+{
+    class Test
+    {
+        public void f1()
+        {
+            WriteLine(2);
+        }
+    }
+}
+
+namespace CS_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            fs.Test t1 = new fs.Test();
+            t1.f1();
+
+            ss.Test t2 = new ss.Test();
+            t2.f1();
+        }
+    }
+}
---
+++ day1/Test/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections;
+
+namespace Test
+{
+    class Star
+    {
+        public int id;
+        public int Id { get; set; }
+        public int age;
+        public int Age { get; set; }
+        public Star(int id, int age)
+        {
+            this.id = id;
+            this.age = age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            ArrayList al = new ArrayList();
+            int ct = 0;
+            while (true)
+            {
+                //조건에 따라서 랜덤하게 데이터 삭제
+                for (int i = 0; i < al.Count;)
+                {
+                    if(rand.Next(2) == 0)
+                    {
+                        al.RemoveAt(i);
+                    }
+                    else
+                    {
+                        ((Star)al[i]).age++;
+                        i++;
+                    }
+                }
+
+                //데이터 추가
+                while (al.Count < 10) 
+                {
+                    al.Add(new Star(ct++,0));
+                }
+
+                foreach (Star item in al) Write("{0:d2} ", item.id);
+                WriteLine();
+                foreach (Star item in al) Write("{0:d2} ", item.age);
+                WriteLine();
+                //키 입력조건
+                Write("숫자를 입력하세요 exit = 99 : ");
+                int input = int.Parse(ReadLine());
+                WriteLine();
+                if (input == 99) break;
+            }
+        }
+    }
+}
+
