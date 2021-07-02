863a7a05fcc016672cfbe890b4ada75d5cbe87ac
Bbabang-1227 <guswns2811@gmail.com>
2021-07-02 17:58:34 +0900

    주성 수업
    1

---
+++ day2/4.Math/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections;
+
+namespace aboutMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 직각삼각형의 사이각이 30도일때 빗변 / 높이 = sin30
+
+            // 기본상식으로는 1/2가 나와야하는데 안나온다
+            // 파이, 라디안 * 3.14 = 180도
+            // 30도라고생각하지마라 30라디안이라고 생각해라
+            // 30 * 3.14 
+            // 180 : 3.14 = 30 : ?
+            WriteLine(Math.Sin(DegreeToRadian(30.0)));
+
+            float sum = 0.0f;
+            for (int i = 0; i < 100; i++)
+            {
+                sum += 0.1f;
+            }
+
+            WriteLine(sum);
+
+            // 
+
+            //Dictionary 검색특화
+            //{1,2} 1.key 2.value
+            Hashtable user = new Hashtable();
+            user.Add("1", "방현준");
+            user.Add("2", "아르카나");
+            user.Add("3", "데모닉");
+            WriteLine(user.Count);
+
+            //제거
+            user.Remove("2");
+            WriteLine(user.Count);
+
+            //Delete all
+            //user.Clear();
+            //WriteLine(user.Count);
+
+            //값을 가지고 있는가? 검색
+            WriteLine(user.Contains("1"));
+            WriteLine(user.Contains("2"));
+
+            WriteLine("---------------------------");
+            //read
+            WriteLine(user["1"]);
+            WriteLine(user["2"]);
+            WriteLine("---------------------------");
+
+            user.Add("4", "바드");
+            ICollection keys = user.Keys;
+            ICollection values = user.Values;
+
+            foreach ( var key in keys)
+            {
+                Write(key + " ");
+            }WriteLine();
+
+            foreach (var value in values)
+            {
+                Write(value + " ");
+            }
+            WriteLine();
+            WriteLine("---------------------------");
+            //데이터 갱신 update
+            user["1"] = "스카우터";
+            user["2"] = "워로드";
+            foreach (var value in values)
+            {
+                Write(value + " ");
+            }
+            WriteLine();
+
+            //
+            WriteLine("---------------------------");
+            WriteLine(user["2"]);
+            string s = user["2"].ToString();
+
+            foreach (DictionaryEntry value in user)
+            {
+                //Write("{0} \t {1}", value.Key, value.Value);
+                
+                WriteLine($"{value.Key} \t {value.Value}");
+            }
+            WriteLine();
+
+            Clear();
+
+            foreach (var item in user.Values)
+            {
+                WriteLine(item);
+            }
+
+            WriteLine(user.ContainsValue("바드"));
+
+            //Key값은 고유해야한다 중복x
+            //user.Add("1", "중복키에데이터넣어보기");
+            Clear();
+
+
+            //SortedList
+            //키값을 기준으로 정렬
+            SortedList sl = new SortedList();
+            sl.Add("나", "이즈리얼");
+            sl.Add("가", "진");
+            sl.Add("다", "카르마");
+            sl.Add("라", "세트");
+
+            foreach (DictionaryEntry item in sl)
+            {
+                WriteLine($"{item.Key} \t {item.Value}");
+            }
+
+
+            WriteLine("---------------------------");
+            //데이터 갱신
+            sl.Add("마", "초가스");
+            sl["마"] = "럭스";
+
+            foreach (DictionaryEntry item in sl)
+            {
+                WriteLine($"{item.Key} \t {item.Value}");
+            }
+
+            WriteLine("---------------------------");
+            sl.SetByIndex(1,"블리츠크랭크");
+
+            foreach (DictionaryEntry item in sl)
+            {
+                WriteLine($"{item.Key} \t {item.Value}");
+            }
+
+            WriteLine("---------------------------");
+            //삭제
+            sl.RemoveAt(0);
+            foreach (DictionaryEntry item in sl)
+            {
+                WriteLine($"{item.Key} \t {item.Value}");
+            }
+            WriteLine("---------------------------");
+            WriteLine(sl.GetByIndex(2));
+            Clear();
+
+            //ILIST
+            IList ilkeys = sl.GetKeyList();
+            IList ilvalue = sl.GetValueList();
+            foreach (var item in values)
+            {
+
+            }
+        }
+        static double DegreeToRadian(double degree)
+        {
+            return Math.PI*degree / 180.0;
+        }
+    }
+}
---
+++ day2/5.0702/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections;
+
+namespace _5._0702
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue
+            Queue q = new Queue();
+            q.Enqueue("Msg1");
+            q.Enqueue("Msg2");
+            q.Enqueue("Msg3");
+            WriteLine(q.Count);
+
+            while (q.Count != 0)
+            {
+                string s = q.Dequeue().ToString();
+                WriteLine(q.Count + "" + s);
+            }
+            WriteLine("exit");
+            q.Enqueue("Msg1");
+            q.Enqueue("Msg2");
+            q.Enqueue("Msg3");
+            string s2 = q.Peek().ToString();
+            WriteLine(q.Count + " " + s2);
+            //큐를 비운다
+            //q.Clear();
+
+            object[] ar = q.ToArray();
+            WriteLine(q.Count);
+            WriteLine("---------------------");
+            foreach (string item in ar)
+            {
+                WriteLine(item);
+            }
+            q.Clear();
+            ArrayList al = new ArrayList();
+            al.Add("MSG 10");
+            al.Add("MSG 20");
+            al.Add("MSG 30");
+
+            q.Enqueue(al);
+            q.Clear();
+            Clear();
+            //Stack
+            Stack st = new Stack(al);
+            WriteLine(st.Count);
+            st.Push(40);
+            st.Push(50);
+            WriteLine("----------------------");
+            WriteLine(st.Count);
+            while (st.Count != 0)
+            {
+                object ob = st.Pop();
+                Write(ob + " ");
+                WriteLine(st.Count);
+            }
+
+            st.Push(10);
+            st.Push(20);
+            st.Push(30);
+            //peek = 맨 위의 개체를 스텍에서 삭제하지않고 출력
+            int num = (int)st.Peek();
+            WriteLine(num);
+
+            WriteLine("--------스택에서 삭제하지않고 데이터추출------");
+            object[] ob2 = st.ToArray();
+            foreach (object item in ob2)
+            {
+                WriteLine(item);
+            }
+            WriteLine(st.Count);
+            Clear();
+            WriteLine("----------------------");
+            //bitarray
+            BitArray barr = new BitArray(new bool[] { true, false, true, false });
+
+            foreach (bool item in barr)
+            {
+                WriteLine(item);
+            }
+
+            barr.Not();
+            WriteLine("----------------------");
+            foreach (bool item in barr)
+            {
+                WriteLine(item);
+            }
+
+            WriteLine("----------------------AND");
+            BitArray barr2 = new BitArray(new bool[] { true, false, true, false });
+            BitArray barr3 = new BitArray(new bool[] { true, false, true, false });
+            foreach (bool item in barr2.And(barr3))
+            {
+                WriteLine(item);
+            }
+            WriteLine("----------------------OR");
+            foreach (bool item in barr2.Or(barr3))
+            {
+                WriteLine(item);
+            }
+            WriteLine("----------------------XOR");
+            foreach (bool item in barr2.Xor(barr3))
+            {
+                WriteLine(item);
+            }
+            Clear();
+
+            f5("방현준" , 23 ,"방현준");
+
+            string str = "스트링타입";
+            char[] carr = str.ToCharArray();
+            foreach (object item in carr)
+            {
+                Write(item);
+            }WriteLine();
+            Array.Reverse(carr);
+            WriteLine(carr);
+            Clear();
+
+            WriteLine(carr.Length);
+            WriteLine(carr);
+            swap(carr);
+
+            Clear();
+
+            //GUID 생성
+            object[] idob = new object[10];
+            for (int i = 0; i < 10; i++)
+            {
+                //option N , D(기본) , B , P
+                idob[i] = Guid.NewGuid().ToString("P");
+            }
+            foreach (object item in idob)
+            {
+                WriteLine(item);
+            }
+            WriteLine("----------------------");
+            Clear();
+
+            Random ran = new Random();
+            int o = 0;
+            int omax = 0;
+            int x = 0;
+            int xmax = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if(ran.Next(0,2) % 2 == 0)
+                {
+                    ++o;
+                    if (omax < o)
+                    {
+                    omax = o;
+                    }
+                    x = 0;
+                    Write("O");
+                }
+                else
+                {
+                    ++x;
+                    if (xmax < x)
+                    {
+                        xmax = x;
+                    }
+                    o = 0;
+                    Write("X");
+                }
+            }WriteLine();
+            WriteLine(omax + " " + xmax);
+        }
+
+        //params[] 를 전달인수로 가지고있는 함수
+        // 함수 호출시 지정된 전달인수를 제외하고 추가적으로 전달할게 있다면 유용할듯
+        static void f5(string s , params object[] p)
+        {
+            foreach (object item in p)
+            {
+                WriteLine(item);
+            }
+        }
+
+        static void swap(char[] carr)
+        {
+            for (int i = 0; i <= carr.Length / 2; i++)
+            {
+                char temp = carr[i];
+
+                carr[i] = carr[carr.Length - i - 1];
+                carr[carr.Length - i - 1] = temp;
+            }
+            WriteLine(carr);
+        }
+    }
+}
